7404c9677051682cd27347be0a9dfa815629dd54
dashingshakeel <shkeelrauf@gmail.com>
2016-10-24 19:28:04 +0500

    Added update view

--- app.js
+++ app.js
@@ -5,22 +5,41 @@ var Post = Backbone.Model.extend({
   }
 });
 
-var Posts = Backbone.Collection.extend({
-});
-
-var post1 = new Post({
-  title: 'shakeel',
-  description: 'nothing'
-});
+var Posts = Backbone.Collection.extend({});
 
 
-var posts = new Posts([post1]);
+
+var posts = new Posts();
 
 var PostView = Backbone.View.extend({
   model: new Post(),
   tagName: 'tr',
   initialize: function() {
     this.template = _.template($('.posts-list-template').html());
+  },
+  events: {
+    'click .edit-post': 'edit',
+    'click .update-post': 'update',
+    'click .cancel': 'cancel',
+    'click .delete-post': 'delete'
+  },
+  edit: function() {
+    $('.edit-post').hide();
+    $('.delete-post').hide();
+    this.$('.update-post').show();
+    this.$('.cancel').show();
+
+    var title = this.$('.title').html();
+    var description = this.$('.description').html();
+    
+
+    this.$('.title').html('<input type="text" class="form-control title-update" value="' + title + '">');
+    this.$('.title').html('<input type="text" class="form-control description-update" value="' + description + '">');
+
+  },
+  update: function() {
+    this.model.set('title', $('.title-update').val());
+    this.model.set('description', $('.description-update').val());
   },
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
@@ -47,6 +66,7 @@ var PostsView = Backbone.View.extend({
 });
 
 var postsView = new PostsView();
+
 $(document).ready(function(){
   $('.add-post').on('click',function(){
     var post = new Post({
